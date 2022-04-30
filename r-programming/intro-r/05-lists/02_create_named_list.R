9c69afdc216ce3c0fd0d8c6c36bbf971fcfee73a
Hossam Khair <hossam.khir1@outlook.com>
2022-04-30 16:50:58 +0200

    feat: learned about lists

---
+++ r-programming/intro-r/05-lists/02_create_named_list.R
@@ -0,0 +1,23 @@
+mov <- "The Shining"
+act <-
+  c("Jack Nicholson",
+    "Shelley Duvall",
+    "Danny Lloyd",
+    "Scatman Crothers",
+    "Barry Nelson")
+
+scores <- c(4.5, 4, 5)
+sources <- c("IMDb1", "IMDb2", "IMDb3")
+comments <-
+  c(
+    "Best Horror Film I Have Ever Seen",
+    "A truly brilliant and scary film from Stanley Kubrick",
+    "A masterpiece of psychological horror"
+  )
+
+rev <- data.frame(scores, sources, comments)
+
+# Finish the code to build shining_list
+shining_list <- list(moviename = mov,
+                     actors = act,
+                     reviews = rev)
---
+++ r-programming/intro-r/05-lists/03_selecting_list_element.R
@@ -0,0 +1,8 @@
+source("./05-lists/02_create_named_list.R")
+
+# Print out the vector representing the actors
+actors <- shining_list$actors
+actors
+
+# Print the second element of the vector representing the actors
+actors[2]
