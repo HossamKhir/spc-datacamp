2ecf849549a5340a78df7b9bfc44faa15ee63ae6
Hossam Khair <hossam.khir1@outlook.com>
2022-05-06 22:26:33 +0200

    feat: learned about conditionals & control flow w/ if, if-else, if-else-if-else

---
+++ r-programming/intermediate-r/01-conditionals-ctrl-flow/06_blend_it_together.R
@@ -0,0 +1,10 @@
+li_df <- read.csv("./li_df.csv", row.names = 1)
+
+# Select the second column, named day2, from li_df: second
+second <- li_df$day2
+
+# Build a logical vector, TRUE if value in second is extreme: extremes
+extremes <- second > 25 | second < 5
+
+# Count the number of TRUEs in extremes
+sum(extremes)
