3540b2560b04ebe747129aa475519f1aa1083378
Hossam Khair <hossam.khir1@outlook.com>
2022-08-16 17:43:09 +0200

    feat: learned about group_by, summarize, & practiced scatter plot with ggplot + geom_point + expand_limits

---
+++ r-programming/intro-tidyverse/03_grouping_summarising/05_summarising_by_continent.R
@@ -0,0 +1,9 @@
+library(gapminder)
+library(dplyr)
+
+# Find median life expectancy and maximum GDP per capita in each continent in 1957
+gapminder %>%
+  filter(year == 1957) %>%
+  group_by(continent) %>%
+  summarize(medianLifeExp = median(lifeExp),
+            maxGdpPercap = max(gdpPercap))
